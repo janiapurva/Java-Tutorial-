0c62f9b5e7223c9ab4ec4be701d78dd07d6c342c
janiapurva <jani.apurva@yahoo.com>
2021-05-20 17:15:16 -0400

    primitve value type

--- src/main/java/com/helloworld/App.java
+++ src/main/java/com/helloworld/App.java
@@ -7,10 +7,22 @@ package com.helloworld;
 public class App 
 {
     public static void main( String[] args ) {
+
+        //Primitive data value
         //Intialize value in variable small digit with byte 
         byte age = 30;
         //larger digit with int sepertate with _ for value
         int counter = 3_233;
+       // for bigger integer value can be store by long
+       long largeNumber = 3334_33442_24242_4424L;
+       //store value in decimal point use double/flot
+       float price = 10.99F;
+
+       //store charecter use char
+       char letter = 'A';
+       //boolean  variable
+       boolean isEligible = false;
+
         System.out.println( age );
     }
 }
